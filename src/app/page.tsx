075e53d5422f4cbf1d143b76b391495ce7593fc5
betrayal019 <business@opuskeys.com>
2026-03-12 12:34:52 +0100

    Remove discounted games and featured banner from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import FeaturedBanner from '@/components/FeaturedBanner';
-import DiscountedSidebar from '@/components/DiscountedSidebar';
-
 export default function HomePage() {
   return (
-    <main className="flex gap-5 px-10 py-[30px] max-w-[1400px] mx-auto">
-      <FeaturedBanner />
-      <DiscountedSidebar />
+    <main className="px-10 py-[30px] max-w-[1400px] mx-auto">
     </main>
   );
 }
